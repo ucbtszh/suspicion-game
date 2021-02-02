6599b05e668df353776a27b40ac1b5b08609552e
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2021-02-02 20:32:29 +0100

    refactored specs to simpler and cleaner flow

--- game_interactive.py
+++ game_interactive.py
@@ -1,8 +1,7 @@
-import random
 from specs import Trial, Game
 
 # define trial parameters
-n_red = [int(float(n)) for n in input("Specify number of red cards for each consecutive trial, separated by comma: ").split(',')]
+n_red = [int(float(n)) for n in input("Specify number of red cards for each consecutive trial, separated by a comma: ").split(',')]
 
 random_trials = input("Do you want to randomize the trials order? (y/n)")
 if random_trials == "y":
@@ -12,16 +11,12 @@ if random_trials == "n":
 else:
     pass
 
-outcome = []
-for i in range(len(n_red)):
-    outcome.append(random.choice([1, -1]))
 #todo: add user input for n_trials?
 
 # create trials
-trial_params = list(zip(n_red, outcome))
 trials = []
-for params in trial_params:
-    trials.append(Trial(params))
+for n in n_red:
+    trials.append(Trial(n))
 
 # create and play game
 game = Game(trials, player=None, n_trials=None, randomize=random_trials)
--- specs.py
+++ specs.py
@@ -1,27 +1,54 @@
 import random
 import csv
+
 from time import time
+from datetime import datetime
 
 random.seed(42)
 
-def suspicion(pre_suspicion, alpha, baseline, outcome, expectation) -> float:
-    """
-    Returns suspicion level according to hypothesized computational behaviour model.
-    """
-    return pre_suspicion + alpha * (outcome - expectation) + baseline
+# helper functions
+def delta(x):
+    if len(x) > 1:
+        return [y - x for x, y in zip(x, x[1:])]
+    return "The game first needs to be played before temporal differences can be computed: Game.play() or Game.play_simulate()"
+
+def reward(card_opponent, card_player):
+    if card_opponent == 1 and card_player == -1:
+        print("You lose 1 point")
+        return -1
+    elif card_opponent == -1 and card_player == 1:
+        print("You win 1 point")
+        return 1
+    elif card_opponent == 1 and card_player == 1:
+        print("You and your opponent lose 1 point")
+        return -1
+    elif card_opponent == -1 and card_player == -1:
+        print("You and your opponent win 1 point")
+        return 1
+
+def save_log_as_csv(log, filename):
+    timestr = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+    filename = str(filename) + "-" + timestr + ".csv"
+    with open(filename, "w", encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerows(log)
+    print("Saved file as", filename)
+
 
 class Trial(object):
     """
-    One game play trial is defined by the proportion of red/blue cards over the total number of cards and the opponent's shown outcome.
+    Defines a single trial by the proportion of red/blue cards over the total number of cards.
     """
 
     n_cards = 5
 
-    def __init__(self, trial_params, **kwargs):
-        # minimum requirement for a trial are number of red cards and opponent outcome, expected as trial_params
-        self.n_red = trial_params[0]
-        self.outcome = trial_params[1]
+    def __init__(self, n_red, outcome=None, **kwargs):
+        if n_red > self.n_cards:
+            raise ValueError("Number of red cards cannot exceed total number of cards (5)")
+        self.n_red = n_red  # minimally required setting for a trial
         self.n_blue = self.n_cards - self.n_red
+        self.outcome = outcome if outcome is not None else random.choice([-1, 1])
+        self.exp_violation = self.outcome - self.expectation()
 
         # add any further specified parameters
         for key, value in kwargs:
@@ -49,16 +76,22 @@ class Player(object):
     Player (participant) attributes and possible actions (lie or not lie) based on suspicion model and game rules.
     """
 
-    def __init__(self, player_params, pre_suspicion=None, **kwargs):
-        # todo: estimate alpha and baseline from actual playing behaviour - add equations
-        self.alpha = player_params[0]
-        self.baseline = player_params[1]
+    def __init__(self, alpha, bias=None, pre_suspicion=None, beta=None, **kwargs):
+        # todo: estimate alpha and bias from actual playing behaviour - add equations
+        self.alpha = alpha
+        self.bias = bias if bias is not None else 0
+        self.pre_suspicion = pre_suspicion if pre_suspicion is not None else 0 # initialize first trial pre_suspicion=0
+        self.beta = beta if beta is not None else 1
 
-        # first trial has no pre_suspicion
-        self.pre_suspicion = pre_suspicion if pre_suspicion is not None else 0
+        # add any further specified parameters
+        for key, value in kwargs:
+            self.key = value
 
     def update_suspicion(self, value):
         self.pre_suspicion = value
+
+    def update_bias(self, value):
+        self.bias = value
 
 
 class Game(object):
@@ -68,98 +101,143 @@ class Game(object):
 
     def __init__(self, trials, player=None, n_trials=None, randomize=False):
         self.trials = trials  # expected to be of class Trial
-        if player is not None:
-            self.player = player  # expected to be of class Player
-            self.player_suspicion = [self.player.pre_suspicion]
-        self.n_trials = n_trials if n_trials is not None else len(trials) # todo: update self.trials if n_trials < or > len(trials)
+        self.player = player # expected to be of class Player
+        self.n_trials = n_trials if n_trials is not None \
+            else len(trials)  # todo: update self.trials if n_trials < or > len(trials)
+
         if randomize:
             random.shuffle(trials)
-        self.player_log = []
 
-    def play_simulate(self, verbose=True):
-        """ Simulate gameplay with given trial and player settings. """
+        self.sim_log = ["trial",
+                        "n_red",
+                        "n_blue",
+                        "opponent_card",
+                        "expectation",
+                        "exp_violation",
+                        "suspicion_tmin1",
+                        "learning_rate",
+                        "bias",
+                        "delta_suspicion",
+                        "suspicion_t",
+                        "player_pick",
+                        "played_card",
+                        "reward_t"]
+
+        self.live_log = ["trial",
+                        "n_red",
+                        "n_blue",
+                        "opponent_card",
+                        "expectation",
+                        "exp_violation",
+                        "player_pick",
+                        "played_card",
+                        "reward_t",
+                         "RT",
+                         "rating"]
+
+    def simulate_play(self, verbose=True):
+        """ Simulate gameplay with given trials and player settings."""
         if verbose:
             def verboseprint(*args):
                 print(*args)
         else:
             verboseprint = lambda *a: None
 
+        print("starting game play simulation")
+        verboseprint("player attributes: bias: ", self.player.bias, "alpha: ", self.player.alpha)
+
         for index, t in enumerate(self.trials, start=1):
             print("trial: ", index)
-            verboseprint("player attributes: baseline: ", self.player.baseline, "alpha: ", self.player.alpha,
-                         "pre-suspicion: ", self.player.pre_suspicion)
+            verboseprint("suspicion_t:", self.player.pre_suspicion)
             verboseprint("# red cards: ", t.n_red)
 
-            # define playing options
             selected_card = t.selected_card()
-            verboseprint("card selected for player: ", selected_card)
-            lie_card = 1 if selected_card == -1 else -1
+            verboseprint("randomly picked card for player: ", selected_card)
 
-            #todo: equation for when player is going to lie themselves
-            if abs(self.player.alpha * (t.outcome - t.expectation())) > self.player.baseline + self.player.pre_suspicion:
-                player_selection = lie_card
-                print("player lies: ", player_selection)
-            else:
-                player_selection = selected_card
-                print("player plays selected card: ", player_selection)
-
+            # todo: for now, let player randomly "lie" or not - determine based on value-suspicion function
+            player_selection = random.choice([1, -1])
             verboseprint("opponent card: red") if t.outcome == -1 else verboseprint("opponent card: blue")
 
-            if player_selection > t.outcome:
-                print("player wins \n")
-            elif player_selection < t.outcome:
-                print("player loses \n")
-            else:
-                print("it's a tie \n")
+            t.reward = reward(t.outcome, player_selection)
 
-            # update player suspicion and save value
-            new_suspicion = suspicion(self.player.pre_suspicion, self.player.alpha, self.player.baseline, t.outcome,
-                                      t.expectation())
-            self.player_suspicion.append(new_suspicion)
-            self.player.update_suspicion(new_suspicion)
-            self.player_log.append(
-                [index, self.player.alpha, self.player.baseline, t.n_red, t.n_blue, t.outcome, t.expectation(), selected_card, player_selection, new_suspicion])
+            # player suspicion according to suspicion formula
+            new_suspicion = self.player.pre_suspicion + self.player.bias + self.player.alpha * t.exp_violation
+            delta_suspicion = self.player.pre_suspicion - new_suspicion
+            verboseprint("change in suspicion: ", delta_suspicion)
             verboseprint("new player suspicion: ", new_suspicion, "\n")
+
+            # log game
+            self.sim_log.append([index,
+                                 t.n_red,
+                                 t.n_blue,
+                                 t.outcome,
+                                 t.expectation(),
+                                 t.exp_violation,
+                                 self.player.pre_suspicion,
+                                 self.player.alpha,
+                                 self.player.bias,
+                                 delta_suspicion,
+                                 new_suspicion,
+                                 selected_card,
+                                 player_selection,
+                                 t.reward])
+            self.player.update_suspicion(new_suspicion)
+        print("end of simulated game")
+        save_input = input("save log? (y/n)")
+        if save_input == "y":
+            filename = input("save as: ")
+            save_log_as_csv(self.sim_log, filename)
 
     def play(self):
         """ Play game using command line interface. """
+        print("=" * 100)
+        print("Welcome to the Lucky Card game! \n"
+              "In each round, you and your opponent will see the identical set of cards. \n"
+              "The computer randomly selects a card for you and your opponent. \n"
+              "You need to decide whether to play this card or to play a card with a different colour. \n"
+              "\n"
+              "Good luck.")
+        print("=" * 100)
+        print("=" * 100)
         for index, t in enumerate(self.trials, start=1):
-            print("You are in trial", index)
-            print("There are", t.n_red, "red cards and", t.n_blue, "blue cards")
             selected_card = t.selected_card()
-            print("Your suggested playing card is red") if selected_card == -1 else print("Your suggested playing card is red")
+
+            print("Round", index)
+            print("There are", t.n_red, "red cards and", t.n_blue, "blue cards")
+            print("Your randomly selected playing card is red") if selected_card == -1 else print(
+                "Your randomly selected playing card is blue")
 
             # catch plyaer RT and store decision
             starttime = time()
-            player_selection = input("Which colour do you wish to play? (red/blue)")
+            player_selection = input("Which card do you wish to play? (red/blue)")
             response_time = time() - starttime
 
             print("You selected red") if player_selection == "red" else print("You selected blue")
             print("Your opponent selected: red card") if t.outcome == -1 else print("Your opponent selected: blue card")
 
-            if player_selection == "blue" and t.outcome == -1:
-                print("player wins \n")
-            elif player_selection == "red" and t.outcome == 1:
-                print("player loses \n")
+            if player_selection == "red":
+                player_selection = -1
             else:
-                print("it's a tie \n")
+                player_selection = 1
+            t.reward = reward(t.outcome, player_selection)
 
-            q_suspect = input("On a scale from 0-10, how confident are you in that your opponent played honestly? (0 = not at all, 10 = completely)" )
+            rating = input(
+                "On a scale from 0-10, how confident are you in that your opponent played honestly? (0 = not at all, 10 = completely)")
 
-            self.player_log.append(
-                [index, t.n_red, t.n_blue, t.outcome, t.expectation(), selected_card, player_selection, response_time,
-                 q_suspect])
-            print("="*100)
+            self.live_log.append([index,
+                                  t.n_red,
+                                  t.n_blue,
+                                  t.outcome,
+                                  t.expectation(),
+                                  t.exp_violation,
+                                  selected_card,
+                                  player_selection,
+                                  t.reward,
+                                  response_time,
+                                  rating])
+            print("=" * 100)
         print("You have reached the end of the game.")
-
         save_input = input("Would you like to save your results? (y/n)")
         if save_input == "y":
-            filename = input("Save as: ") + ".csv"
-            with open(filename, "w") as f:
-                writer = csv.writer(f)
-                writer.writerows(self.player_log)
-
-    def delta_suspicion(self):
-        if len(self.player_suspicion) > 1:
-            return [y - x for x, y in zip(self.player_suspicion, self.player_suspicion[1:])]
-        return "The game first needs to be played before suspicion can be developed: Game.play() or Game.play_simulate()"
+            filename = input("Save as: ")
+            save_log_as_csv(self.live_log, filename)
